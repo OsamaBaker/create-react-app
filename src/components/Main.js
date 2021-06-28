987c65d573ffb6da415174dc6678dab8582e8c18
Osama Baker <edewanjy@hotmail.com>
2021-06-28 18:50:58 +0300

    Completed most of the tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import HornedBeasts from './HornedBeasts';
-import '../App.css'
+import React from "react";
+import HornedBeasts from "./HornedBeasts";
+import "../App.css";
 
 class Main extends React.Component {
-    render() {
-        return (
-            <div>
-                <HornedBeasts className='beastImage' title='Thor' imageUrl='https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2017/03/20/876286-366818302.jpg?itok=Qfv0UdK_' description='Blue Beast'/>
 
-                <HornedBeasts className='beastImage' title='Goree' imageUrl='https://daily.jstor.org/wp-content/uploads/2021/04/king_kong_mole_rats_and_the_mark_of_the_beast_1050x700.jpg' description='Gorilla Beast'/>
 
-            </div>
-        )
-    }
+
+
+  render() {
+    return (
+      <div>
+        {this.props.beasts.map((item) => {
+          return (
+            <HornedBeasts
+              title={item.title}
+              image_url={item.image_url}
+              description={item.description}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Main;
