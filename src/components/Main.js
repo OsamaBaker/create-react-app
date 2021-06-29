b4fcc672659d8ecac578aa921fdbaab7279a868e
Osama Baker <edewanjy@hotmail.com>
2021-06-29 20:21:41 +0300

    Completed the tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,27 @@
 import React from "react";
 import HornedBeasts from "./HornedBeasts";
 import "../App.css";
+// import SelectedBeast from "./SelectedBeast";
+
 
 class Main extends React.Component {
-
-
 
 
   render() {
     return (
       <div>
-        {this.props.beasts.map((item) => {
+        {this.props.beasts.map((item, index) => {
           return (
             <HornedBeasts
               title={item.title}
               image_url={item.image_url}
               description={item.description}
-            />
-          );
-        })}
+              key={index}
+              modal={this.props.showBeast}
+              />
+              );
+            })}
+            
       </div>
     );
   }
