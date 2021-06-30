59f9a6c33ac72549a0951d9e3550074ac00f61b9
Osama Baker <edewanjy@hotmail.com>
2021-06-30 20:14:01 +0300

    Last

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,50 @@
 import React from "react";
 import HornedBeasts from "./HornedBeasts";
 import "../App.css";
-// import SelectedBeast from "./SelectedBeast";
-
+import HornsForm from "./HornsForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Beasts from "./Beasts.json";
+import Form from "react-bootstrap/Form";
 
 class Main extends React.Component {
 
+  handleChange = (event) => {
+    event.preventDefault();
+    let value = event.target.value;
+
+    this.props.filter(value);
+  };
 
   render() {
     return (
-      <div>
+      <>
+        <Form onSelect={this.handleChange}>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Select number of horns:</Form.Label>
+            <Form.Control
+              as="select"
+              name="select"
+              onChange={this.handleChange}
+            >
+              <option value="all" name="all">
+                All
+              </option>
+              <option value="1" name="one">
+                1
+              </option>
+              <option value="2" name="two">
+                2
+              </option>
+              <option value="3" name="three">
+                3
+              </option>
+              <option value="100" name="oneHundred">
+                Wow..
+              </option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <HornsForm />
         {this.props.beasts.map((item, index) => {
           return (
             <HornedBeasts
@@ -18,11 +53,10 @@ class Main extends React.Component {
               description={item.description}
               key={index}
               modal={this.props.showBeast}
-              />
-              );
-            })}
-            
-      </div>
+            />
+          );
+        })}
+      </>
     );
   }
 }
